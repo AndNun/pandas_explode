913f9b96015fd35e7b1414ce68fc7187c02db27f
AndNun <andsonunes@gmail.com>
2025-04-13 13:17:34 -0300

    ajustes filtro cliente

--- pandas_explode.py
+++ pandas_explode.py
@@ -6,7 +6,8 @@ import numpy as np
 # %%
 # Importando dados de exemplo
 dados_transacao = pd.read_csv('transacao_cartao.txt', sep = ';')
-dados_transacao.info()
+#dados_transacao = dados_transacao.query('id_cliente == 1')
+dados_transacao.head()
 
 # %%
 # Tratando data da transação
@@ -16,11 +17,11 @@ dados_transacao['dt_transacao']
 # %%
 # Criando coluna contendo uma lista com o valor das parcelas
 dados_transacao['valor_parcela'] = dados_transacao.apply(lambda row: [row['valor']/row['qtd_parcelas'] for i in range(row['qtd_parcelas'])], axis = 1)
-
+dados_transacao.head()
 # %%
 # Aplicando função explode para expandir o valor para parcela para cada transação
 dados_fatura = dados_transacao.explode('valor_parcela')
-
+dados_fatura.head()
 # Removendo colunas desnecessárias
 dados_fatura = dados_fatura.drop(['valor', 'qtd_parcelas'], axis = 1)
 dados_fatura
